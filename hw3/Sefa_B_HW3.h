2f5473b291f300ce0712ed9760f998bf81a22de5
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-11-07 22:19:29 -0500

    Fixed spaceing issue

--- hw3/Sefa_B_HW3.h
+++ hw3/Sefa_B_HW3.h
@@ -14,7 +14,7 @@ class node{
 		node* link(){ return link_field; }
 		const node* link() const{ return link_field; } /*NOTE this function is constant because a constant pointer can call it, where it doesn't want to change any of its values in the future. */
 		node(const nodeDataType& init_data = nodeDataType(), 
-				node* init_link = NULL){
+		node* init_link = NULL){
 			link_field = init_link;
 			data_field = init_data;
 		}
